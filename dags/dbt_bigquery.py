d38fbe1512d2c038e52c2086d4823aec8d17748f
Rafa <jr.vera.ma@gmail.com>
2024-03-07 14:49:17 +0100

    quick fix

--- dags/dbt_bigquery.py
+++ dags/dbt_bigquery.py
@@ -23,7 +23,7 @@ with airflow.DAG(
         image='jrvm/dbt_bigquery:dbt-image',
         cmds=["dbt", "run"],
         arguments=[
-            "--project-dir", "/dbt_bigquery_main", "--profiles-dir", "/dbt_bigquery_main/profiles"
+            "--project-dir", "/dbt/dbt_bigquery", "--profiles-dir", "/dbt/dbt_bigquery/profiles"
         ],
         name="dbt_transformations",
         task_id="dbt_transformations",
