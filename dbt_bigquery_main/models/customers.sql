9f045a1bf33cbe72ddc4a5e7e4dba4c7d18c3276
Rafa <jr.vera.ma@gmail.com>
2024-03-08 18:59:44 +0100

    dbt new model

---
+++ dbt_bigquery_main/models/customers.sql
@@ -0,0 +1,41 @@
+with customers as (
+        select id,
+            first_name,
+            last_name
+        from 'dbt-tutorial'.jaffle_shop.customers
+
+),
+
+orders as (
+    select id,
+           user_id,
+           order_date,
+           status
+    from 'dbt-tutorial'.jaffle_shop.orders
+),
+
+customer_orders as (
+        SELECT
+            user_id,
+            min(order_date) as first_order,
+            max(order_date) as most_recent_order,
+            count(id) as number_of_orders
+
+         from orders
+         group by user_id
+),
+
+final as (
+    select
+        customers.id,
+        customer.first_name,
+        customers.last_name,
+        customer_orders.first_order,
+        customer_orders.most_recent_order,
+        customer_orders.number_of_orders
+    from customers
+    left join customer_orders
+    on customers.id = customer_orders.user_id
+)
+
+select * from final
--- dbt_bigquery_main/models/example/my_first_dbt_model.sql
+++ dbt_bigquery_main/models/example/my_first_dbt_model.sql
@@ -1,12 +1,4 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
 with source_data as (
@@ -20,8 +12,5 @@ with source_data as (
 select *
 from source_data
 
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
 
 -- where id is not null
